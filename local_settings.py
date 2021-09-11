cce2a112cade197d7c30e267fdbb37de68228a5d
Thea Barnes <thea@tsbarnes.com>
2021-09-11 15:33:51 -0400

    More work on apps and controls

--- local_settings.py
+++ local_settings.py
@@ -1,3 +1,5 @@
 import logging
 
 LOGLEVEL = logging.DEBUG
+FONT = "/usr/share/fonts/truetype/dejava/DejaVuSansMono.ttf"
+MONOSPACE_FONT = "/usr/share/fonts/truetype/dejava/DejaVuSansMono.ttf"
--- pitftmanager.py
+++ pitftmanager.py
@@ -3,7 +3,7 @@ import logging
 import time
 import sys
 from types import ModuleType
-
+import posix_ipc
 import settings
 from utils import wrapped_text
 from framebuffer import Framebuffer
@@ -14,10 +14,14 @@ class PiTFTManager:
     framebuffer: Framebuffer = Framebuffer(1)
     app_modules: list = []
     apps: list = []
+    current_app_index: int = 0
     current_app_module: ModuleType = None
     current_app: AbstractApp = None
 
     def __init__(self):
+        self.mq = posix_ipc.MessageQueue("/pitftmanager_ipc", flags=posix_ipc.O_CREAT)
+        self.mq.block = False
+
         app_names = settings.APPS
         for name in app_names:
             try:
@@ -37,11 +41,43 @@ class PiTFTManager:
         for module in self.app_modules:
             self.apps.append(module.App(self.framebuffer))
 
-        self.current_app_module = self.app_modules[0]
-        self.current_app = self.apps[0]
+        self.switch_app(0)
+
+    def switch_app(self, index):
+        self.current_app_index = index % len(self.apps)
+        self.current_app_module = self.app_modules[self.current_app_index]
+        self.current_app = self.apps[self.current_app_index]
+
+    def previous_app(self):
+        self.switch_app(self.current_app_index - 1)
+
+    def next_app(self):
+        self.switch_app(self.current_app_index + 1)
 
     def main_loop(self):
         while True:
+            try:
+                message = self.mq.receive(timeout=10)
+            except posix_ipc.BusyError:
+                message = None
+
+            if message:
+                parts = message[0].decode().split()
+                command = parts[0]
+                args = " ".join(parts[1:])
+
+                logging.info("Received IPC command: " + command)
+
+                if command == "previous":
+                    self.previous_app()
+                elif command == "next":
+                    self.next_app()
+                elif command == "switch_app":
+                    self.switch_app(args)
+                elif command == "exit":
+                    logging.info("Got 'exit' command, quitting...")
+                    sys.exit(0)
+
             self.current_app.run_iteration()
             time.sleep(1)
 
@@ -50,7 +86,9 @@ if __name__ == '__main__':
     print("Starting PiTFT Manager...")
     print("Log Level: " + logging.getLevelName(settings.LOGLEVEL))
     logging.basicConfig(level=settings.LOGLEVEL)
+
     app = PiTFTManager()
     image = wrapped_text("Starting PiTFT Manager...", app.framebuffer.size, font_size=40, background_color="black")
     app.framebuffer.show(image)
+
     app.main_loop()
