fabac4e327006926caaadebb62d9bc3bc460b18f
Thea Barnes <thea@tsbarnes.com>
2021-09-17 06:36:34 -0400

    Improve the dashboard

--- apps/dashboard.py
+++ apps/dashboard.py
@@ -18,26 +18,26 @@ class App(AbstractApp):
         self.text(text, font_size=48, position=(60, 5))
 
         text = str(self.weather.weather.current.sky_text)
-        self.text(text, font_size=14, position=(150, 20))
+        self.text(text, font_size=20, position=(150, 20))
 
         self.line((0, 60, self.image.size[0], 60), width=2)
 
         if len(self.calendar.events) > 0:
             start = self.calendar.standardize_date(self.calendar.events[0]["start"])
             text = self.calendar.humanized_datetime(start)
-            self.text(text, font_size=16, position=(5, 60))
+            self.text(text, font_size=24, position=(5, 60), color="orange")
 
             text = str(self.calendar.events[0]['summary'])
-            self.text(text, font_size=14, position=(5, 80), wrap=True, max_lines=2)
+            self.text(text, font_size=24, position=(5, 90), wrap=True, max_lines=2)
 
-        self.line((0, 120, self.image.size[0], 120), width=2)
+        self.line((0, 160, self.image.size[0], 160), width=2)
 
         if len(self.calendar.tasks) > 0:
             text = str(self.calendar.tasks[0]['summary'])
-            self.text(text, font_size=14, position=(5, 130), wrap=True, max_lines=2)
+            self.text(text, font_size=24, position=(5, 170), wrap=True, max_lines=2)
 
     def touch(self, position: tuple):
-        if position[0] in range(10, 50):
-            if position[1] in range(10, 50):
+        if position[0] in range(0, 60):
+            if position[1] in range(0, 60):
                 logging.debug("Weather icon touched")
         logging.debug("Position {} touched".format(position))
