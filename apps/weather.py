9a125ac3f34d8bd7d31ec2b01c84a60d0a162e26
Thea Barnes <thea@tsbarnes.com>
2021-09-21 18:27:11 -0400

    Improved weather view

--- apps/weather.py
+++ apps/weather.py
@@ -11,19 +11,16 @@ class App(AbstractApp):
         self.draw_titlebar("Weather")
 
         try:
-            centered_position: int = round(self.framebuffer.size[0] / 2 - 40)
-            box: tuple = (centered_position, 5)
-
             text = str(self.weather.weather.current.temperature) + '°'
-            self.wrapped_text(text, font_size=80, position=(centered_position, 30))
+            self.centered_text(text, font_size=80, y=30)
 
             text = str(self.weather.weather.current.sky_text)
-            self.wrapped_text(text, font_size=40, position=(centered_position, 120))
+            self.centered_text(text, font_size=40, y=120)
 
             text = str(self.weather.weather.current.day)
-            self.wrapped_text(text, font_size=40, position=(centered_position, 170))
+            self.centered_text(text, font_size=40, y=170)
 
             logging.debug("Sky Code: " + str(self.weather.weather.current.sky_code))
         except AttributeError:
             text = "No weather information"
-            self.wrapped_text(text, font_size=30, position=(5, 5))
+            self.centered_text(text, font_size=30, y=120)
