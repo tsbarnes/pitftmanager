b22dbd033cd22fbb219065301606e6a1a0b8a1fa
Thea Barnes <thea@tsbarnes.com>
2021-10-16 14:42:04 -0400

    Remove unneeded code from sensors app

--- apps/sensors.py
+++ apps/sensors.py
@@ -1,18 +1,10 @@
 """Sensors screen"""
 from apps import AbstractApp
-import sensors
 from libs.system import System, get_system
 
 
 class App(AbstractApp):
     system: System = get_system()
-
-    def __init__(self, fb):
-        """
-        Initialize sensors library
-        """
-        sensors.init()
-        super().__init__(fb)
 
     def reload(self):
         self.blank()
